7dcd8f1c3a0a4bd9e797c7322b1de9cce26aee74
Antonius Naumann <antonius.naumann@student.hpi.de>
2020-11-21 17:12:18 +0100

    Add validation check for author to have a last name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Author, type: :model do
     author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "wikipedia.org/Alan_Turing")
     expect(author.first_name).to eq("Alan")
   end
+
+  it 'should not validate if no last_name is specified' do
+    author = Author.new(first_name: "Grace", homepage: "grace-hopper.com")
+    expect(author).to_not be_valid
+  end
 end
